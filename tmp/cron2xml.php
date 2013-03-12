1c1aa199091c4dcfcd7ad67604fcce9b789a7bcd
comsi02 <comsi02@gmail.com>
2013-03-13 00:41:55 +0900

    working...

--- tmp/cron2xml.php
+++ tmp/cron2xml.php
@@ -6,6 +6,9 @@ function cron2xml()
     $jenkins_base = '/home/comsi02/work/jenkins';
     $jenkins_url  = 'http://10.2.8.101:9100';
 
+    $jenkins_base = '/home/comsi02/local/jenkins';
+    $jenkins_url  = 'http://192.168.0.18:9100';
+
     $cmd = "java -jar $jenkins_base/home/war/WEB-INF/jenkins-cli.jar -s $jenkins_url list-jobs --username comsi02 --password fsdfsd";
     $cmd = "java -jar $jenkins_base/home/war/WEB-INF/jenkins-cli.jar -s $jenkins_url list-jobs";
 
@@ -26,12 +29,29 @@ function cron2xml()
     $cron_file_info = file_get_contents("$jenkins_base/cron/bbat.tmonc.net.cron",true);
 
     $ss = explode("\r\n",$cron_file_info); 
+
+	$pattern_job = '/^\# job\|/';
+	$pattern_cmd = '/^\# cmd\|/';
+	$job_hash;
+	$cmd_hash;
+
     foreach ($ss as $s)
     {
-        var_dump($s);
+		if (preg_match($pattern_job,$s)) {
+			$r = explode("|",$s);
+        	var_dump($r);
+			$job_hash[$r[1]] = $r[2];
+		}
+
+		if (preg_match($pattern_cmd,$s)) {
+			$r = explode("|",$s);
+        	var_dump($r);
+			$cmd_hash[$r[1]] = $r[2];
+		}
     }
 
-
+	var_dump($job_hash);
+	var_dump($cmd_hash);
 
 
 }
