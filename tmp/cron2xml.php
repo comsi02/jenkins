4b2ff923010e5c0484750ab211085c23a7106ab3
comsi02 <comsi02@gmail.com>
2013-03-14 10:50:55 +0900

    cron2xml.php 개발

--- tmp/cron2xml.php
+++ tmp/cron2xml.php
@@ -1,60 +1,127 @@
 <?php
 
+function jenkins_job_list($jenkins_cmd){ /*{{{*/
+    $job_list = array();
 
-function cron2xml()
-{
-    $jenkins_base = '/home/comsi02/work/jenkins';
-    $jenkins_url  = 'http://10.2.8.101:9100';
+    exec($jenkins_cmd.'list-jobs',$result,$retval);
 
-    $jenkins_base = '/home/comsi02/local/jenkins';
-    $jenkins_url  = 'http://192.168.0.18:9100';
-
-    $cmd = "java -jar $jenkins_base/home/war/WEB-INF/jenkins-cli.jar -s $jenkins_url list-jobs --username comsi02 --password fsdfsd";
-    $cmd = "java -jar $jenkins_base/home/war/WEB-INF/jenkins-cli.jar -s $jenkins_url list-jobs";
-
-    exec($cmd,$result,$retval);
-
-    if($retval == 0) {
-        echo "----------------------------------------\n";
-        var_dump($result);
-        echo "----------------------------------------\n";
-    } else {
+    if($retval != 0) {
         print "cron2xml.php error!!!\n";
         die(1);
     }
-
-    #$fh = fopen("$jenkins_base/cron/bbat.tmonc.net.cron","r");
-    #var_dump($fh);
-
-    $cron_file_info = file_get_contents("$jenkins_base/cron/bbat.tmonc.net.cron",true);
-
-    $ss = explode("\r\n",$cron_file_info); 
-
-	$pattern_job = '/^\# job\|/';
-	$pattern_cmd = '/^\# cmd\|/';
-	$job_hash;
-	$cmd_hash;
-
-    foreach ($ss as $s)
-    {
-		if (preg_match($pattern_job,$s)) {
-			$r = explode("|",$s);
-        	var_dump($r);
-			$job_hash[$r[1]] = $r[2];
-		}
-
-		if (preg_match($pattern_cmd,$s)) {
-			$r = explode("|",$s);
-        	var_dump($r);
-			$cmd_hash[$r[1]] = $r[2];
-		}
+    
+    foreach ($result as $job_name) {
+        $job_list[$job_name] = 'AV';
     }
 
-	var_dump($job_hash);
-	var_dump($cmd_hash);
+    return $job_list;
+} /* }}} */
 
+function cron2xml() { /* {{{ */
 
-}
+    $jenkins_base = '/home/comsi02/work/jenkins';
+    $jenkins_url  = 'http://10.2.8.101:9100';
+    $jenkins_cmd  = "/usr/bin/java -jar $jenkins_base/home/war/WEB-INF/jenkins-cli.jar -s $jenkins_url ";
+
+    $module = array('job','cmd','sms','eml');
+    $cron_info = array();
+
+    $cron_info['jenkins_list'] = jenkins_job_list($jenkins_cmd);
+    $cron_cont = explode("\r\n",file_get_contents("$jenkins_base/cron/bbat.tmonc.net.cron",true)); 
+
+    foreach ($cron_cont as $line) {
+        foreach ($module as $m) {
+            $pattern = '/^\# '.$m.'\|/';
+		    if (preg_match($pattern,$line)) {
+			    $res = explode("|",$line,3);
+			    $cron_info[$m][$res[1]] = $res[2];
+		    }
+        }
+    }
+
+    #var_dump($cron_info);
+
+    foreach ($cron_info['job'] as $job_name => $job_info) {
+
+        list($job_status, $job_desc) = explode("|",$cron_info['job'][$job_name],2);
+        list($job_schedule, list($job_cmd)) = array_chunk((preg_split ("/\s+/", $cron_info['cmd'][$job_name],6)),5);
+
+        $job_schedule = implode(" ",$job_schedule)."\n# $job_desc";
+
+        $job_noti_eml = str_replace(";"," ",$cron_info['eml'][$job_name]);
+
+        if ($job_noti_eml == null) {
+            $job_noti_eml = str_replace(";"," ",$cron_info['eml']['default']);
+        }
+
+        $job_noti_sms = str_replace(";"," ",$cron_info['sms'][$job_name]);
+
+        if ($job_noti_sms == null) {
+            $job_noti_sms = str_replace(";"," ",$cron_info['sms']['default']);
+        }
+
+        if ($job_status == 'AV') {
+            $job_status = 'false';
+        } else {
+            $job_status = 'true';
+        }
+
+        $jenkins_cli_cmd = 'create-job';
+        if ($cron_info['jenkins_list'][$job_name] == 'AV') {
+            $jenkins_cli_cmd = 'update-job';
+        }
+
+        $job_templete = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<project>
+  <actions/>
+  <description>$job_desc</description>
+  <logRotator class=\"hudson.tasks.LogRotator\">
+    <daysToKeep>1</daysToKeep>
+    <numToKeep>-1</numToKeep>
+    <artifactDaysToKeep>-1</artifactDaysToKeep>
+    <artifactNumToKeep>-1</artifactNumToKeep>
+  </logRotator>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class=\"hudson.scm.NullSCM\"/>
+  <canRoam>true</canRoam>
+  <disabled>$job_status</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers class=\"vector\">
+    <hudson.triggers.TimerTrigger>
+      <spec>$job_schedule</spec>
+    </hudson.triggers.TimerTrigger>
+  </triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell>
+      <command>$job_cmd</command>
+    </hudson.tasks.Shell>
+  </builders>
+  <publishers>
+    <hudson.tasks.Mailer plugin=\"mailer@1.4\">
+      <recipients>$job_noti_eml</recipients>
+      <dontNotifyEveryUnstableBuild>false</dontNotifyEveryUnstableBuild>
+      <sendToIndividuals>false</sendToIndividuals>
+    </hudson.tasks.Mailer>
+  </publishers>
+  <buildWrappers/>
+</project>";
+
+        print "#--------------------[ $job_name ]--------------------#\n";
+
+        exec("$jenkins_cmd $jenkins_cli_cmd $job_name <<< '$job_templete'\n",$result,$retval);
+
+        print_r(array($job_name,$job_desc,$job_status,str_replace("\n"," ",$job_schedule),$job_cmd,$job_noti_eml));
+
+        if ($retval == 0) {
+            echo "[SUCC] : job create success.\n";
+        } else {
+            echo "[FAIL] : job create failure.\n";
+        }
+    }
+} /* }}} */
 
 cron2xml();
 ?>
